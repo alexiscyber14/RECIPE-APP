0b9d6c8be711d2b7e1ea1a6eb28bbe924835ed7f
Yosefgeda <100337086+Yosefgeda@users.noreply.github.com>
2023-07-03 15:50:31 +0300

    associations created

---
+++ db/migrate/20230703123456_add_index_to_recipe_food_on_recipe_id.rb
@@ -0,0 +1,4 @@
+class AddIndexToRecipeFoodOnRecipeId < ActiveRecord::Migration[7.0]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema[7.0].define(version: 2023_07_03_094145) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_03_123711) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -42,10 +41,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_03_094145) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-
-ActiveRecord::Schema[7.0].define(version: 2023_07_03_083019) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name", default: "", null: false
@@ -64,7 +60,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_03_083019) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["name"], name: "index_users_on_name", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-
   end
+
+  add_foreign_key "foods", "users", column: "user_id"
+  add_foreign_key "recipes", "users", column: "user_id"
 
 end

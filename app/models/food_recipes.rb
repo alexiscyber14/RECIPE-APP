d1b9184e308feb25737112eae8b95836dfb1ee33
alexiscyber14 <142sledgehammer14@gmail.com>
2023-07-06 04:37:01 +0100

    fix rubo linter

--- app/models/food_recipes.rb
+++ app/models/food_recipes.rb
@@ -1,7 +1,6 @@
 class FoodRecipe < ApplicationRecord
-    belongs_to :recipe
-    belongs_to :food
-  
-    validates :quantity, presence: true, numericality: { greater_than_or_equal_to: 0 }
-  end
-  
+  belongs_to :recipe
+  belongs_to :food
+
+  validates :quantity, presence: true, numericality: { greater_than_or_equal_to: 0 }
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,10 +1,5 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
-require 'rails_helper'
-
 RSpec.describe Food, type: :model do
   include Devise::Test::IntegrationHelpers
   before :each do
